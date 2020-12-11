a6253a430d663a724f0f7362f3bf3106e569c0df
wenzstev <wenzelstev@gmail.com>
2020-12-11 16:03:13 -0600

    communcation between front and backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
+const cors = require("cors")
+
 var app = express()
+app.use(cors())
+
 
 app.get('/', (req, res)=>res.json("Hello frontend!"));
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
 
-function App() {
+
+import axios from "axios"
+
+const App = () => {
+  const [message, setMessage] = useState("")
+
+  useEffect(()=>{
+    axios.get("http://localhost:5000/")
+      .then(res=>setMessage(res.data))
+      .catch(err=>console.log(err))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>And now, an important message from the backend:</h1>
+      <h2>{message ? message: "Loading..."}</h2>
     </div>
-  );
+    )
 }
 
 export default App;
